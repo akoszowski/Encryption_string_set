9360e568e95f6b7d6faf424d3146c604e74f42d5
akoszowski <a.koszowski@students.mimuw.edu.pl>
2020-11-05 18:53:26 +0100

    Interfejs i szablon jego implementacji

--- encstrset.cc
+++ encstrset.cc
@@ -1,6 +1,87 @@
+#include "encstrset.h"
 #include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef NDEBUG
+    static const bool debug = true;
+#else
+    static const bool debug = false;
+#endif
+
+#define DIAGNOSTIC_PRINT1(info) do { \
+    if (!debug) { std::cerr << info << ": "; } \
+    } while(0)
+
+#define DIAGNOSTIC_PRINT2(info) do { \
+    if (!debug) { std::cerr << info << std::endl;} \
+    } while(0)
+
+
+namespace {
+    // Tu zmienne globalne i funkcje pomocnicze
+    using std::unordered_map;
+    using std::unordered_set;
+    using std::stringstream;
+    using std::string;
+
+    unsigned long cur_id = 0;
+
+    string info;
+    stringstream sstream;
+
+    unordered_map<unsigned long, unordered_set<string>> map_of_sets;
+
+}
+
+unsigned long jnp1::encstrset_new() {
+    DIAGNOSTIC_PRINT1("encstrset_new");
+
+    unordered_set<string> new_set;
+    map_of_sets[cur_id] = new_set;
+
+    sstream << "set #" << cur_id << " created";
+    info = sstream.str();
+    DIAGNOSTIC_PRINT2(info);
+
+    return cur_id++;
+}
+
+void jnp1::encstrset_delete(unsigned long id) {
+    //
+}
+
+size_t jnp1::encstrset_size(unsigned long id) {
+    //
+}
+
+bool jnp1::encstrset_insert(unsigned long id, const char *value,
+                            const char *key) {
+    //
+}
+
+bool jnp1::encstrset_remove(unsigned long id, const char *value,
+                            const char *key) {
+    //
+}
+
+bool jnp1::encstrset_test(unsigned long id, const char *value,
+                          const char *key) {
+    //
+}
+
+void jnp1::encstrset_clear(unsigned long id) {
+    //
+}
+
+void jnp1::encstrset_copy(unsigned long src_id, unsigned long dst_id) {
+    //
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    jnp1::encstrset_new();
+
+
     return 0;
 }
--- encstrset.h
+++ encstrset.h
@@ -1,6 +1,57 @@
 #ifndef ENCSTRSET_H
 #define ENCSTRSET_H
 
+#ifndef __cplusplus
+    #include <stdlib.h>
+#endif
 
+#ifdef __cplusplus
+    #include <cstdlib>
+
+    extern "C" {
+
+    namespace jnp1 {
+#endif
+
+// Creates new set. Returns its [id].
+        unsigned long encstrset_new();
+
+// If set with [id] exists, it is removed. Nothing happens otherwise.
+        void encstrset_delete(unsigned long id);
+
+// If set with [id] exists, returns its number of elements, 0 otherwise.
+        size_t encstrset_size(unsigned long id);
+
+// If set with [id] exists and element [value] after encoding it with
+// [cipher] is not a part of the set, then the element is added [true];
+// otherwise nothing happens [false].
+        bool
+        encstrset_insert(unsigned long id, const char *value, const char *key);
+
+// If set with [id] exists and element [value] after encoding it with
+// [cipher] is a part of the set, then the element is removed [true];
+// otherwise nothing happens [false].
+        bool
+        encstrset_remove(unsigned long id, const char *value, const char *key);
+
+// If set with [id] exists and element [value] after encoding it with
+// [cipher] is a part of the set, then returns [true];
+// otherwise returns [false].
+        bool
+        encstrset_test(unsigned long id, const char *value, const char *key);
+
+// If set with [id] exists, removes its all elements;
+// otherwise nothing happens.
+        void encstrset_clear(unsigned long id);
+
+// If both set with [src_id] and [dst_id] exist, content of the first one
+// is copied into the second one; otherwise nothing happens.
+        void encstrset_copy(unsigned long src_id, unsigned long dst_id);
+
+#ifdef __cplusplus
+    }
+
+    }
+#endif
 
 #endif //ENCSTRSET_H
